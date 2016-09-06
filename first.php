1f49ee59f66f8200d8c9894e142ea33978fef42e
andralla <andralla@greeny.cs.tlu.ee>
2016-09-06 15:16:52 +0300

    added first php file

---
+++ first.php
@@ -0,0 +1,15 @@
+
+<?php
+
+	$firstName = "Andres";
+	$lastName = "Alla";
+
+	
+	echo $firstName;
+	echo " ";
+	echo $lastName;
+	
+	// punkt liidab stringe
+	
+	// echo $firstName. " " .$lastName
+?>
